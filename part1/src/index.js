7e12fd234f95f19094b8f1dd41f689499b2c12bb
Nitish Raju <nitishraju@outlook.com>
2020-12-21 18:20:17 -0500

    Solved Ex 1.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,64 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course.name}</h1>
-    </div>
-  )
-}
+const Button = ({text, clickHandler}) => (
+  <button onClick={clickHandler}>
+    {text}
+  </button>
+)
 
-const Part = (props) => {
-  return (
-    <>
-    <p>{props.part.name}: {props.part.exercises}</p>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part part={props.course.parts[0]} />
-      <Part part={props.course.parts[1]} />
-      <Part part={props.course.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>Total number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </div>
-  )
-}
+const Stat = ({text, count}) => <p>{text} {count}</p>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
 
   return (
     <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total parts={course.parts} />
+      <h1>give feedback</h1>
+      <div>
+        <Button text="good" clickHandler={incrementGood} />
+        <Button text="neutral" clickHandler={incrementNeutral} />
+        <Button text="bad" clickHandler={incrementBad} />
+      </div>
+      <h1>statistics</h1>
+      <div>
+        <Stat text="good" count={good} />
+        <Stat text="neutral" count={neutral} />
+        <Stat text="bad" count={bad} />
+      </div>
     </div>
   )
 }
