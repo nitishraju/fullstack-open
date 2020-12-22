5ab17128f4274f9130edc4bc1f985b757d348009
Nitish Raju <nitishraju@outlook.com>
2020-12-21 20:25:40 -0500

    Solved Ex 1.12

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,71 +2,34 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const Button = ({text, clickHandler}) => (
-  <button onClick={clickHandler}>
-    {text}
-  </button>
+  <div>
+    <button onClick={clickHandler}>{text}</button>
+  </div>
 )
 
-const Stat = ({text, value}) => (
-  <tr>
-    <td>{text}</td>
-    <td>{value}</td>
-  </tr>
-)
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const incrementGood = () => setGood(good + 1)
-  const incrementNeutral = () => setNeutral(neutral + 1)
-  const incrementBad = () => setBad(bad + 1)
-
-  const getTotal = () => good + neutral + bad
-  const getPositivePct = () => isNaN(good/getTotal()) ? "N/A" : (good/getTotal())*100 + "%"
-  const getAvg = () => (getTotal() === 0) ? 0 : (good-bad)/getTotal()
-
-  if (getTotal() === 0) {
-    return (
-      <div>
-        <h1>give feedback</h1>
-        <div>
-          <Button text="good" clickHandler={incrementGood} />
-          <Button text="neutral" clickHandler={incrementNeutral} />
-          <Button text="bad" clickHandler={incrementBad} />
-        </div>
-        <div>
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-        </div>
-      </div>
-    )
-  }
+  const setRandomIndex = () => setSelected(Math.floor(Math.random()*props.anecdotes.length))
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <div>
-        <Button text="good" clickHandler={incrementGood} />
-        <Button text="neutral" clickHandler={incrementNeutral} />
-        <Button text="bad" clickHandler={incrementBad} />
-      </div>
-      <h1>statistics</h1>
-      <div>
-        <table>
-          <tbody>
-            <Stat text="good" value={good} />
-            <Stat text="neutral" value={neutral} />
-            <Stat text="bad" value={bad} />
-            <Stat text="all" value={getTotal()} />
-            <Stat text="average" value={getAvg()} />
-            <Stat text="positive" value={getPositivePct()} />
-          </tbody>
-        </table>
-      </div>
+      <p>{props.anecdotes[selected]}</p>
+      <Button text="next anecdote" clickHandler={setRandomIndex} />
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
