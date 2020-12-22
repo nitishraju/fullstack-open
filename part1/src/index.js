764859b0d0e993869ce8b89160891de4d84a093e
Nitish Raju <nitishraju@outlook.com>
2020-12-21 21:10:55 -0500

    Solved Ex 1.14

--- part1/src/index.js
+++ part1/src/index.js
@@ -5,21 +5,49 @@ const Button = ({text, clickHandler}) => <button onClick={clickHandler}>{text}</
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
-  const [votes, setVotes] = useState(props.anecdotes.map((x) => 0))
+  const [voteCounts, setVotes] = useState(props.anecdotes.map((x) => 0))
+  const [mostVotes, setMostVotes] = useState(-1)
 
   const incrementVote = (selected) => () => {
-    const copy = [...votes]
+    const copy = [...voteCounts]
     copy[selected] += 1
     setVotes(copy)
+
+    checkHighestVotes(copy[selected])
   }
+
+  const checkHighestVotes = (currentVotes) => {
+    if (mostVotes === -1 || currentVotes > voteCounts[mostVotes]) {
+      setMostVotes(selected)
+    }
+  }
+
   const setRandomIndex = () => setSelected(Math.floor(Math.random()*props.anecdotes.length))
+
+  if (mostVotes === -1) {
+    return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {voteCounts[selected]} votes</p>
+      <Button text="vote" clickHandler={incrementVote(selected)} />
+      <Button text="next anecdote" clickHandler={setRandomIndex} />
+      <h1>Anecdote with most votes</h1>
+      <p>No votes yet!</p>
+    </div>
+    )
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
-      <p>has {votes[selected]} votes</p>
+      <p>has {voteCounts[selected]} votes</p>
       <Button text="vote" clickHandler={incrementVote(selected)} />
       <Button text="next anecdote" clickHandler={setRandomIndex} />
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[mostVotes]}</p>
+      <p>has {voteCounts[mostVotes]} votes</p>
     </div>
   )
 }
