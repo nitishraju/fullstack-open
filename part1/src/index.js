2ced725bd35412330ed9e48ba047a819f50e8ee6
Nitish Raju <nitishraju@outlook.com>
2020-12-24 15:14:53 -0500

    Solved Ex 2.9

--- part1/src/index.js
+++ part1/src/index.js
@@ -4,17 +4,21 @@ import ReactDOM from 'react-dom'
 const Persons = ({personObject}) => {
   return (
     <div>
-    <p>{personObject.name} {personObject.phone}</p>
+    <p>{personObject.name} {personObject.number}</p>
     </div>
   )
 }
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', phone: "123-456-7890" }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [filter, setFilter] = useState('')
 
   const nameHandler = (event) => setNewName(event.target.value)
   const phoneHandler = (event) => setNewPhone(event.target.value)
@@ -38,9 +42,16 @@ const App = () => {
     setNewPhone('')
   }
 
+  const filterHandler = (event) => setFilter(event.target.value)
+  const personsToShow = persons.filter((personObject) => personObject.name.toUpperCase().includes(filter.toUpperCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={filterHandler} />
+      </div>
+      <h2>Add a new person</h2>
       <form onSubmit={submitHandler} >
         <div>
           name: <input value={newName} onChange={nameHandler}/>
@@ -54,7 +65,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((personObject) => 
+        {personsToShow.map((personObject) => 
           <Persons key={personObject.name} personObject={personObject} />
         )}
       </div>
