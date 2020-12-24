169f3a80cd67ff4cda11f4b2f299c14fd4779af7
Nitish Raju <nitishraju@outlook.com>
2020-12-23 22:15:41 -0500

    Solved Ex 2.6

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,55 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import Course from './components/Course'
+
+const Persons = ({name_obj}) => {
+  return (
+    <div>
+    <p>{name_obj.name}</p>
+    </div>
+  )
+}
 
 const App = () => {
-  const courses = [
-    {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
-        {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
-        },
-        {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
-        },
-        {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
-        },
-        {
-          name: 'Redux',
-          exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
-    {
-      name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
 
-  return <Course courses={courses} />
+  const changeHandler = (event) => setNewName(event.target.value)
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+
+    const noteObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(noteObject))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={submitHandler} >
+        <div>
+          name: <input value={newName} onChange={changeHandler}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((name_obj) => 
+          <Persons key={name_obj.name} name_obj={name_obj} />
+        )}
+      </div>
+    </div>
+  )
 }
+
+export default App
 
 ReactDOM.render(
   <App  />,
